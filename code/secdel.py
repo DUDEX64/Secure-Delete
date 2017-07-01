823a0caa4b71de3ec26084d39c8a4cc04ad3b4e2
DUDEX64 <theprogrammer3@gmail.com>
2017-07-02 04:15:24 +1000

    Wrote basic function in code/secdel.py

--- code/secdel.py
+++ code/secdel.py
@@ -2,10 +2,22 @@
 import os
 import sys
 
-def ReplaceWithZeros(): 
-    return
+def ReplaceWithZeros(filename): 
+    if not os.path.isfile(filename): 
+        return false
+    ...
+    fd = open(filename, "wb")
+    size = os.path.getsize(filename)
+    n = 1
+    while (n <= size) :
+        fd.write(bytes(500))
+        n += 500
+    ...
+    fd.flush()
+    fd.close()
+    return true
 ...
-def SecureDelete(): 
+def SecureDelete(argv): 
     return
 ...
 
